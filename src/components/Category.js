0628f06801579bc7bc026da0d6f43d5d693e734e
Apoorv Shaswat <shaswatapoorv@gmail.com>
2024-10-13 19:31:41 +0530

    responsive

--- src/components/Category.js
+++ src/components/Category.js
@@ -10,14 +10,14 @@ export default function Category() {
   };
 
   return (
-    <div className="flex flex-col gap-5 p-5 bg-yellow-200">
+    <div className="flex flex-col gap-5 p-0 md:p-5 bg-yellow-200">
       {quizData.map((category, index) => (
-        <div className="border p-4 border-yellow-600" key={index}>
+        <div className=" p-4 " key={index}>
           <div className="text-xl font-bold">{category.category}</div>
-          <div className="flex overflow-x-auto gap-5 py-2">
+          <div className="flex overflow-x-auto gap-4 py-2">
             {category.quizzes.map((quiz) => (
               <div
-                className="flex flex-col justify-center text-center gap-5 border p-4 h-52 w-72 border-yellow-600 flex-shrink-0"
+                className="flex flex-col justify-start text-center border h-auto w-52 md:w-72 border-yellow-600 flex-shrink-0"
                 key={quiz.id}
               >
                 <img
@@ -27,7 +27,7 @@ export default function Category() {
                   onClick={() => handleQuizSelect(quiz.id)}
                 />
                 <button
-                  className="text-lg "
+                  className="text-lg md:p-5 flex justify-center items-center"
                   onClick={() => handleQuizSelect(quiz.id)}
                 >
                   {quiz.title}
--- src/components/Header.js
+++ src/components/Header.js
@@ -3,7 +3,7 @@ import { Link } from "react-router-dom";
 
 export default function Header() {
   return (
-    <div className="flex justify-between items-center bg-yellow-400 h-[10vh] md:p-5 px-10">
+    <div className="flex justify-between items-center bg-yellow-400 h-[10vh] md:p-5 px-5">
       <div className="text-2xl md:text-3xl font-bold">Quiz</div>
       <ul className="flex space-x-2 md:space-x-5 text-xl md:text-3xl">
         <li>
